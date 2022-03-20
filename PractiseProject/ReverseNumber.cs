d9ef290c70a1f62057fd06496f94878c081afaed
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-21 00:13:47 +0530

    [Madhavee] Adding ReverseNumber.cs

---
+++ PractiseProject/ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProject
+{
+    class ReverseNumber
+    {
+        
+        public static void FindReverseofNumber(int number)
+        {
+            int reverse = 0, actualnumber;
+            actualnumber = number;
+            while (number > 0)
+            {
+                reverse = (reverse * 10);
+                reverse = reverse + (number % 10);
+                number = number / 10;
+            }
+            Console.WriteLine($"The reverse of {actualnumber} is : {reverse}");
+        }
+    }
+}
