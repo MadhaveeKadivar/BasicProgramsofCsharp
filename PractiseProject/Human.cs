2ada62ca902b932521f799304e7fcbd8b49300c1
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-21 00:15:58 +0530

    [Madhavee] Adding Human.cs

---
+++ PractiseProject/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProject
+{
+    internal class Human
+    {
+        public string Name = "Madhavee";
+        public long phonenumber = 9999999999;
+
+        public static void Speak()
+        {
+            Console.WriteLine("He/She can speak");
+        }
+        public void HumanDetails()
+        {
+            Console.WriteLine($"Name : {Name}  PhoneNumber : {phonenumber}");
+        }
+    }
+}
