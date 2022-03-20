c5bfe648c8614868e461057a9c93a765ab1d9c33
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-21 00:19:27 +0530

    [Madhavee] Adding GCD_LCM.cs

---
+++ PractiseProject/GCD_LCM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProject
+{
+    internal class GCD_LCM
+    {
+        public static void GCD(int no1,int no2)
+        {
+            int gcd=1 ;
+            for (int i = 1; i <= no1 && i <= no2; i++)
+            {
+                if (no1 % i == 0 && no2 % i == 0)
+                {
+                    gcd = i;
+                }
+
+            }
+            Console.WriteLine($"GCD of {no1} and {no2} is : {gcd}");
+
+        }
+        public static void LCM(int no1, int no2)
+        {
+            int gcd = 1,lcm=0;
+            for (int i = 1; i <= no1 && i <= no2; i++)
+            {
+                if (no1 % i == 0 && no2 % i == 0)
+                {
+                    gcd = i;
+                }
+            }
+            lcm = (no1 * no2) / gcd;
+            Console.WriteLine($"LCM of {no1} and {no2} is : {lcm}");
+
+        }
+    }
+}
