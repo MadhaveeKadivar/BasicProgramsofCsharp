b5c57347a5a77779c61c8907b98e1f312363de64
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-21 00:17:32 +0530

    [Madhavee] Adding Factorial.cs

---
+++ PractiseProject/Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProject
+{
+    class Factorial
+    {
+      
+        public static void FindFactorial(int n)
+        {
+            int fact = 1;
+
+           
+
+            if (n == 0 || n == 1)
+            {
+                Console.WriteLine($"Factorial of {n} is : 1");
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine($"Factorial of {n} is : {fact}");
+            }
+           
+        }
+    }
+}
